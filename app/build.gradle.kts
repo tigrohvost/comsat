2be21103ee9cf901e1967ce029ea1cbc64cd1f56
tigrohvost <tigrohvost@gmail.com>
2026-09-07 11:44:57 +0300

    test: allow compiler-only annotations in the release smoke APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            testProguardFiles("proguard-test-rules.pro")
         }
     }
 
---
+++ app/proguard-test-rules.pro
@@ -0,0 +1,3 @@
+# Error Prone annotations in AndroidX Test mention this JDK compiler type.
+# It is not an Android runtime dependency. This rule applies only to the test APK.
+-dontwarn javax.lang.model.element.Modifier
